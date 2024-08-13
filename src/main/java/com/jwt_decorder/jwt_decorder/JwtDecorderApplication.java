4f26e31ddc7028db1be373bae35930db8845f76a
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-08-13 23:31:23 +0530

    decord jwt token using apache codec

--- src/main/java/com/jwt_decorder/jwt_decorder/JwtDecorderApplication.java
+++ src/main/java/com/jwt_decorder/jwt_decorder/JwtDecorderApplication.java
@@ -2,7 +2,15 @@ package com.jwt_decorder.jwt_decorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.UnsupportedEncodingException;
+
+@RestController("/jwt")
 @SpringBootApplication
 public class JwtDecorderApplication {
 
@@ -10,4 +18,13 @@ public class JwtDecorderApplication {
 		SpringApplication.run(JwtDecorderApplication.class, args);
 	}
 
+	@PostMapping("/decode")
+	public String decorodeJwt(@RequestBody String token) throws UnsupportedEncodingException {
+		// header.payload.signature
+		String header = token.split("\\.")[0];
+		String payload = token.split("\\.")[1];
+		String signature = token.split("\\.")[2];
+
+		return new String(Base64.decodeBase64(payload), "UTF-8");
+	}
 }
