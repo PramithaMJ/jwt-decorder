d1cc89da5a9b73f336bfc1a04d482f0a212a6c4e
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-08-13 23:21:17 +0530

    initial setup

---
+++ src/main/java/com/jwt_decorder/jwt_decorder/JwtDecorderApplication.java
@@ -0,0 +1,13 @@
+package com.jwt_decorder.jwt_decorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDecorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt_decorder/jwt_decorder/JwtDecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt_decorder.jwt_decorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
